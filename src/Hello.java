a8e1772bd43f80d297786ecf25c3e0a1e7ea3f50
Chompou <chompouang22@gmail.com>
2025-02-05 13:02:36 +0700

    Create a new Hello class

---
+++ src/Hello.java
@@ -0,0 +1,5 @@
+public class Hello {
+    void hello(){
+        System.out.println("Hello");
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        Hello obj = new Hello();
+        obj.hello();
     }
 }
